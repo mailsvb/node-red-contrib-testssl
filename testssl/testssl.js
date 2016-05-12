38962d45df80381c3ca7de0839f7721503fcd3e3
mailsvb <mailsvb@gmail.com>
2016-05-12 11:20:13 +0200

    verify host address and port are valid. provide intermediate feedback

--- testssl/testssl.js
+++ testssl/testssl.js
@@ -8,6 +8,7 @@ module.exports = function(RED) {
     var fs = require('fs');
     var aha = require('aha');
     var moment = require('moment');
+    var validator = require("validator");
     var spawn = require('child_process').spawn;
 
     function TestSSLScan(n) {
@@ -25,6 +26,46 @@ module.exports = function(RED) {
                 node.status({fill:"red",shape:"dot",text:'address missing'});
                 return;
             }
+            
+            // checking host and port provided
+            var tmpHost = '';
+            var tmpPort = 443;
+            var tmp1 = host.match(/^[A-Za-z0-9\x2E\x2D]*$/);        // check for host only
+            var tmp2 = host.match(/^([^\x3A]*)\x3A([0-9]{1,5})$/);  // check for host + port
+            if (tmp1 instanceof Array && tmp1.length === 1)
+            {
+                tmpHost = tmp1[0];
+            }
+            else if (tmp2 instanceof Array && tmp2.length === 3)
+            {
+                tmpHost = tmp2[1];
+                tmpPort = parseInt(tmp2[2]);
+            }
+            else
+            {
+                node.status({fill:"red",shape:"dot",text:'invalid data given'});
+                msg.payload = "you provided invalid data";
+                node.send(msg);
+                return;
+            }
+            
+            if (validator.isFQDN(tmpHost) === false && validator.isIP(tmpHost, '4') === false)
+            {
+                node.status({fill:"red",shape:"dot",text:'not a valid host'});
+                msg.payload = "this is not a valid host";
+                node.send(msg);
+                return;
+            }
+            if (tmpPort < 1 || tmpPort > 65535)
+            {
+                node.status({fill:"red",shape:"dot",text:'not a valid port'});
+                msg.payload = "this is not a valid port";
+                node.send(msg);
+                return;
+            }
+            
+            msg.payload = "scan has been started...";
+            node.send(msg);
             console.log("[testssl][" + scanID + "] - start scan for host: " + host);
             var timeBefore = moment();
             
@@ -56,6 +97,7 @@ module.exports = function(RED) {
               }
             );
             
+            // global timeout for scan of 20 minutes
             var timeout = setTimeout(function(){
                 if (run != null) {
                     console.log("[testssl][" + scanID + "] - timeout of scan");
@@ -65,6 +107,14 @@ module.exports = function(RED) {
                     return;
                 }
             }, 1200000);
+            
+            // interval to inform user of ongoing activity
+            var interval = setInterval(function(){
+                if (run != null) {
+                    msg.payload = "scan is still running (" + moment().diff(timeBefore, 'seconds') + " seconds)";
+                    node.send(msg);
+                }
+            }, 45000);
             
             run.stdout.on('data', function (data) {
                 HTML+=data.toString('hex');
@@ -79,11 +129,10 @@ module.exports = function(RED) {
                 console.log("[testssl][" + scanID + "] - scan finished of host: " + host);
                 run = null;
                 clearTimeout(timeout);
+                clearInterval(interval);
                 if (error != "") {
                     node.status({fill:"red",shape:"dot",text:'error during scan'});
-                    var msg = {
-                        payload: error.toString().trim().replace(/\[[^m]*m/g,'')
-                    }
+                    msg.payload = error.toString().trim().replace(/\[[^m]*m/g,'');
                     node.send(msg);
                 }
                 else {
@@ -98,9 +147,7 @@ module.exports = function(RED) {
                         start: timeBefore.utc().format(),
                         end: timeAfter.utc().format()
                     }
-                    var msg = {
-                        payload: payload
-                    }
+                    msg.payload = payload;
                     node.status({});
                     node.send(msg);
                 }
