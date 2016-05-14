a2a6bd5089ca2ccc833b796b469b3ae7fb12c3b5
mailsvb <mailsvb@gmail.com>
2016-05-14 23:07:37 +0200

    split scans if FQDN resolves multiple IPs

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ it will download the latest version of the testssl.sh script from github for you
 ## Features
 - scan a host and get the report as text and HTML output
 - use your own OpenSSL version for the scan by providing the path to it
+- multiple IPs for single FQDN will result in seperate reports
--- testssl/testssl.js
+++ testssl/testssl.js
@@ -4,12 +4,13 @@
 
 module.exports = function(RED) {
     "use strict";
-    var util = require('util');
-    var fs = require('fs');
-    var aha = require('aha');
-    var moment = require('moment');
-    var validator = require("validator");
-    var spawn = require('child_process').spawn;
+    const util = require('util');
+    const fs = require('fs');
+    const aha = require('aha');
+    const moment = require('moment');
+    const validator = require("validator");
+    const dns = require('dns');
+    const spawn = require('child_process').spawn;
 
     function TestSSLScan(n) {
         RED.nodes.createNode(this,n);
@@ -22,12 +23,18 @@ module.exports = function(RED) {
             var host    = scantarget || msg.host;
             var path    = opensslpath || msg.opensslpath;
             var scanID  = msg._msgid;
+            
+            // setting path
+            if (path != undefined && path != "") {
+                console.log("[testssl][" + scanID + "] - using openssl executable: " + path);
+                path = '--openssl=' + path;
+            }
+            
+            // checking host and port data provided
             if (host === undefined || host === "") {
                 node.status({fill:"red",shape:"dot",text:'address missing'});
                 return;
             }
-            
-            // checking host and port provided
             var tmpHost = '';
             var tmpPort = 443;
             var tmp1 = host.match(/^[A-Za-z0-9\x2E\x2D]*$/);        // check for host only
@@ -49,13 +56,7 @@ module.exports = function(RED) {
                 return;
             }
             
-            if (validator.isFQDN(tmpHost) === false && validator.isIP(tmpHost, '4') === false)
-            {
-                node.status({fill:"red",shape:"dot",text:'not a valid host'});
-                msg.payload = "this is not a valid host";
-                node.send(msg);
-                return;
-            }
+            // checking port
             if (tmpPort < 1 || tmpPort > 65535)
             {
                 node.status({fill:"red",shape:"dot",text:'not a valid port'});
@@ -64,96 +65,130 @@ module.exports = function(RED) {
                 return;
             }
             
-            msg.payload = "scan has been started...";
-            node.send(msg);
-            console.log("[testssl][" + scanID + "] - start scan for host: " + host);
-            var timeBefore = moment();
-            
-            if (path != undefined && path != "") {
-                console.log("[testssl][" + scanID + "] - using openssl executable: " + path);
-                path = '--openssl=' + path;
-            }
-            
-            var output = "";
-            var HTML = "";
-            var error = "";
-            var timeout = false;
-            
-            node.status({fill:"green",shape:"dot",text:"scanning " + host});
-            
-            var run = spawn('./testssl.sh',
-              [
-                path,
-                '-f',
-                '-p',
-                '-S',
-                '-P',
-                '-U',
-                '-E',
-                host
-              ],
-              {
-                  cwd: __dirname
-              }
-            );
-            
-            // global timeout for scan of 20 minutes
-            var timeout = setTimeout(function(){
-                if (run != null) {
-                    console.log("[testssl][" + scanID + "] - timeout of scan");
-                    node.status({fill:"red",shape:"dot",text:"timeout during scan"});
-                    timeout = true;
-                    run.kill();
+            GetMyTarget(tmpHost, tmpPort, function(hostToScan){
+                if (typeof hostToScan === "string") {
+                    node.status({fill:"red",shape:"dot",text:hostToScan});
+                    msg.payload = hostToScan;
+                    node.send(msg);
                     return;
                 }
-            }, 1200000);
-            
-            // interval to inform user of ongoing activity
-            var interval = setInterval(function(){
-                if (run != null) {
-                    msg.payload = "scan is still running (" + moment().diff(timeBefore, 'seconds') + " seconds)";
-                    node.send(msg);
-                }
-            }, 45000);
-            
-            run.stdout.on('data', function (data) {
-                HTML+=data.toString('hex');
-                output+=data.toString();
-            });
+                
+                node.status({fill:"green",shape:"dot",text:"scanning " + host});
+                msg.payload = "scanning... " + hostToScan.join();
+                node.send(msg);
+                console.log("[testssl][" + scanID + "] - scanning " + host + ": " + hostToScan.join());
+                
+                var timeBefore = moment();
+                
+                hostToScan.forEach(function(curVal, index, arr){
+                
+                    var output = "";
+                    var HTML = "";
+                    var error = "";
+                    var timeout = false;
+                    
+                    var run = spawn('./testssl.sh',
+                      [
+                        path,
+                        '-f',
+                        '-p',
+                        '-S',
+                        '-P',
+                        '-U',
+                        '-E',
+                        curVal
+                      ],
+                      {
+                          cwd: __dirname
+                      }
+                    );
+                    
+                    // timeout for scan of 20 minutes
+                    var timeout = setTimeout(function(){
+                        if (run != null) {
+                            console.log("[testssl][" + scanID + "][" + index + "] - timeout of scan");
+                            node.status({fill:"red",shape:"dot",text:"timeout during scan"});
+                            timeout = true;
+                            run.kill();
+                            return;
+                        }
+                    }, 600000);
+                    
+                    // interval to inform user of ongoing activity
+                    var interval = setInterval(function(){
+                        if (run != null) {
+                            msg.payload = "scan is still running (" + moment().diff(timeBefore, 'seconds') + " seconds) for " + curVal;
+                            node.send(msg);
+                        }
+                    }, 60000);
+                    
+                    run.stdout.on('data', function (data) {
+                        HTML+=data.toString('hex');
+                        output+=data.toString();
+                    });
 
-            run.stderr.on('data', function (data) {
-                error+=data.toString();
-            });
+                    run.stderr.on('data', function (data) {
+                        error+=data.toString();
+                    });
 
-            run.on('close', function(){
-                console.log("[testssl][" + scanID + "] - scan finished of host: " + host);
-                run = null;
-                clearTimeout(timeout);
-                clearInterval(interval);
-                if (error != "") {
-                    node.status({fill:"red",shape:"dot",text:'error during scan'});
-                    msg.payload = error.toString().trim().replace(/\[[^m]*m/g,'');
-                    node.send(msg);
-                }
-                else {
-                    var timeAfter = moment();
-                    var outputHTML = aha(new Buffer(HTML, 'hex'));
-                    var payload = {
-                        text: output,
-                        html: outputHTML,
-                        timeout: timeout,
-                        host: host,
-                        duration: timeAfter.diff(timeBefore, 'seconds'),
-                        start: timeBefore.utc().format(),
-                        end: timeAfter.utc().format()
-                    }
-                    msg.payload = payload;
-                    node.status({});
-                    node.send(msg);
-                }
+                    run.on('close', function(){
+                        console.log("[testssl][" + scanID + "][" + index + "] - scan finished for " + curVal);
+                        run = null;
+                        clearTimeout(timeout);
+                        clearInterval(interval);
+                        if (error != "") {
+                            node.status({fill:"red",shape:"dot",text:'error during scan'});
+                            msg.payload = error.toString().trim().replace(/\[[^m]*m/g,'');
+                            node.send(msg);
+                        }
+                        else {
+                            var timeAfter = moment();
+                            var outputHTML = aha(new Buffer(HTML, 'hex'));
+                            var payload = {
+                                text: output,
+                                html: outputHTML,
+                                timeout: timeout,
+                                host: curVal,
+                                duration: timeAfter.diff(timeBefore, 'seconds'),
+                                start: timeBefore.utc().format(),
+                                end: timeAfter.utc().format()
+                            }
+                            msg.payload = payload;
+                            node.status({});
+                            node.send(msg);
+                        }
+                    });
+                });
             });
         });
     }
 
     RED.nodes.registerType("testssl-node",TestSSLScan);
+    
+    const GetMyTarget = function(tmpHost, tmpPort, cb)
+    {
+        var hostToScan = [];
+        if (validator.isIP(tmpHost, '4') === true)
+        {
+            hostToScan.push(tmpHost + ':' + tmpPort);
+            cb(hostToScan);
+        }
+        else if (validator.isFQDN(tmpHost) === true)
+        {
+            dns.resolve(tmpHost, function(err, addresses){
+                if (err) {
+                    cb("Error resolving host: " + err.message);
+                    return;
+                }
+                for (var i=0; i < addresses.length; i++) {
+                    hostToScan.push(addresses[i] + ':' + tmpPort);
+                }
+                cb(hostToScan);
+            });
+        }
+        else
+        {
+            cb("this is not a valid host");
+        }
+    }
 }
