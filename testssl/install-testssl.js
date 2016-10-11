b02212552978c688e170f121a9e01ea4afd5753c
mailsvb <mailsvb@gmail.com>
2016-10-11 16:17:52 +0200

    switch to drwetter master branch

--- testssl/install-testssl.js
+++ testssl/install-testssl.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var testsslOPTIONS = {
     hostname: 'raw.githubusercontent.com',
     port: 443,
-    path: '/mailsvb/testssl.sh/CA_BUNDLES_PATH/testssl.sh',
+    path: '/drwetter/testssl.sh/master/testssl.sh',
     method: 'GET',
     rejectUnauthorized: false
 };
