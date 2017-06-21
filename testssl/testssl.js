d620fadb1f1d140fa7a0330a42269de17231fc1e
mailsvb <mailsvb@gmail.com>
2017-06-21 10:46:10 +0200

    updates

--- testssl/testssl.js
+++ testssl/testssl.js
@@ -3,6 +3,7 @@ module.exports = function(RED) {
     const util = require('util');
     const fs = require('fs');
     const aha = require('aha');
+    const crypto = require('crypto');
     const moment = require('moment');
     const validator = require("validator");
     const dns = require('dns');
@@ -10,29 +11,29 @@ module.exports = function(RED) {
 
     function TestSSLScan(n) {
         RED.nodes.createNode(this,n);
-        var scantarget      = n.host;
-        var opensslpath     = n.opensslpath || '/usr/bin/openssl';
-        var cabundlespath   = n.cabundlespath || '/etc/ssl';
+        let scantarget      = n.host;
+        let opensslpath     = n.opensslpath || '/usr/bin/openssl';
+        let cabundlespath   = n.cabundlespath || '/etc/ssl';
 
-        var node = this;
+        let node = this;
 
         this.on("input",function(msg) {
-            var host            = scantarget || msg.host;
-            var openssl         = opensslpath || msg.opensslpath;
-            var cabundles       = cabundlespath || msg.cabundlespath;
-            var scanID          = msg._msgid;
-            var env             = Object.create( process.env );
+            let host            = scantarget || msg.host;
+            let openssl         = opensslpath || msg.opensslpath;
+            let capath          = cabundlespath || msg.cabundlespath;
+            let scanID          = msg._msgid;
+            let env             = Object.create( process.env );
             
             // setting openssl path
             if (openssl != undefined && openssl != "") {
-                console.log("[testssl][" + scanID + "] - using openssl executable: " + openssl);
+                node.log(`[testssl][${scanID}] - using openssl executable: ${openssl}`);
                 openssl = '--openssl=' + openssl;
             }
             
             // setting ca-bundles path
-            if (cabundles != undefined && cabundles != "") {
-                console.log("[testssl][" + scanID + "] - using ca bundles path: " + cabundles);
-                env.CA_BUNDLES_PATH = cabundles;
+            if (capath != undefined && capath != "") {
+                node.log(`[testssl][${scanID}] - using ca bundles path: ${capath}`);
+                env.CA_BUNDLES_PATH = capath;
             }
             
             // checking host and port data provided
@@ -40,10 +41,10 @@ module.exports = function(RED) {
                 node.status({fill:"red",shape:"dot",text:'address missing'});
                 return;
             }
-            var tmpHost = '';
-            var tmpPort = 443;
-            var tmp1 = host.match(/^[A-Za-z0-9\x2E\x2D]*$/);        // check for host only
-            var tmp2 = host.match(/^([^\x3A]*)\x3A([0-9]{1,5})$/);  // check for host + port
+            let tmpHost = '';
+            let tmpPort = 443;
+            let tmp1 = host.match(/^[A-Za-z0-9\x2E\x2D]*$/);        // check for host only
+            let tmp2 = host.match(/^([^\x3A]*)\x3A([0-9]{1,5})$/);  // check for host + port
             if (tmp1 instanceof Array && tmp1.length === 1)
             {
                 tmpHost = tmp1[0];
@@ -55,7 +56,7 @@ module.exports = function(RED) {
             }
             else
             {
-                node.status({fill:"red",shape:"dot",text:'invalid data given'});
+                node.status({fill:"red",shape:"dot",text:"invalid data given"});
                 msg.payload = "you provided invalid data";
                 node.send(msg);
                 return;
@@ -64,7 +65,7 @@ module.exports = function(RED) {
             // checking port
             if (tmpPort < 1 || tmpPort > 65535)
             {
-                node.status({fill:"red",shape:"dot",text:'not a valid port'});
+                node.status({fill:"red",shape:"dot",text:"not a valid port"});
                 msg.payload = "this is not a valid port";
                 node.send(msg);
                 return;
@@ -81,28 +82,34 @@ module.exports = function(RED) {
                 node.status({fill:"green",shape:"dot",text:"scanning " + tmpHost + " on port " + tmpPort});
                 msg.payload = "scanning... " + IPsToScan.join() + ' on port ' + tmpPort;
                 node.send(msg);
-                console.log("[testssl][" + scanID + "] - scanning " + tmpHost + " via " + IPsToScan.join() + " on port " + tmpPort);
+                node.log(`[testssl][${scanID}] - scanning ${tmpHost} via ${IPsToScan.join()} on port ${tmpPort}`);
                 
-                var timeBefore = moment();
+                let timeBefore = moment();
                 
-                IPsToScan.forEach(function(singleAddrToScan, index, arr){
+                IPsToScan.forEach((singleAddrToScan, index, arr) => {
                 
-                    var output = "";
-                    var HTML = "";
-                    var error = "";
-                    var timeout = false;
+                    let output = "";
+                    let HTML = "";
+                    let error = "";
+                    let timeout = false;
+                    let hash = crypto.createHash('sha256').update(`${scanID}${tmpHost}${tmpPort}`);
+                    let jsonfile = '/tmp/' + hash.digest('hex');
                     
-                    var run = spawn('./testssl.sh',
+                    let run = spawn('./testssl.sh',
                       [
                         openssl,
-                        '-f',
-                        '-p',
-                        '-S',
-                        '-P',
-                        '-U',
-                        '-E',
-                        '-s',
+                        '--protocols',
+                        '--server-defaults',
+                        '--server-preference',
+                        '--headers',
+                        '--vulnerable',
+                        '--cipher-per-proto',
+                        '--pfs',
+                        '--rc4',
                         '--ip',
+                        '--nodns',
+                        '--jsonfile',
+                        jsonfile,
                         singleAddrToScan,
                         tmpHost + ':' + tmpPort
                       ],
@@ -113,9 +120,9 @@ module.exports = function(RED) {
                     );
                     
                     // timeout for scan of 10 minutes
-                    var timeout = setTimeout(function(){
+                    let timeout = setTimeout(function(){
                         if (run != null) {
-                            console.log("[testssl][" + scanID + "][" + index + "] - timeout of scan");
+                            node.log(`[testssl][${scanID}][${index}] - timeout of scan`);
                             node.status({fill:"red",shape:"dot",text:"timeout during scan"});
                             timeout = true;
                             run.kill();
@@ -124,38 +131,45 @@ module.exports = function(RED) {
                     }, 600000);
                     
                     // interval to inform user of ongoing activity
-                    var interval = setInterval(function(){
+                    let interval = setInterval(() => {
                         if (run != null) {
                             msg.payload = "scan is still running (" + moment().diff(timeBefore, 'seconds') + " seconds) for " + singleAddrToScan + ":" + tmpPort;
                             node.send(msg);
                         }
                     }, 60000);
                     
-                    run.stdout.on('data', function (data) {
+                    run.stdout.on('data', (data) => {
                         HTML+=data.toString('hex');
                         output+=data.toString();
                     });
 
-                    run.stderr.on('data', function (data) {
+                    run.stderr.on('data', (data) => {
                         error+=data.toString();
                     });
 
-                    run.on('close', function(){
-                        console.log("[testssl][" + scanID + "][" + index + "] - scan finished for " + singleAddrToScan + ":" + tmpPort);
+                    run.on('close', () => {
+                        node.log(`[testssl][${scanID}][${index}] - scan finished for ${singleAddrToScan}:${tmpPort}`);
                         run = null;
                         clearTimeout(timeout);
                         clearInterval(interval);
                         if (error != "") {
-                            node.status({fill:"red",shape:"dot",text:'error during scan'});
+                            node.status({fill:"red",shape:"dot",text:"error during scan"});
                             msg.payload = error.toString().trim().replace(/\[[^m]*m/g,'');
                             node.send(msg);
                         }
                         else {
-                            var timeAfter = moment();
-                            var outputHTML = aha(new Buffer(HTML, 'hex'));
-                            var payload = {
+                            let timeAfter = moment();
+                            let outputHTML = aha(new Buffer(HTML, 'hex'));
+                            try {
+                                jsonfile = fs.readFileSync(jsonfile).toString();
+                            }
+                            catch (e) {
+                                node.error(`[testssl][${scanID}][${index}] - ${e}`);
+                            }
+                            let payload = {
                                 text: output,
                                 html: outputHTML,
+                                json: jsonfile,
                                 timeout: timeout,
                                 host: singleAddrToScan,
                                 duration: timeAfter.diff(timeBefore, 'seconds'),
@@ -174,9 +188,8 @@ module.exports = function(RED) {
 
     RED.nodes.registerType("testssl-node",TestSSLScan);
     
-    const GetMyTarget = function(tmpHost, cb)
-    {
-        var IPsToScan    = [];
+    const GetMyTarget = (tmpHost, cb) => {
+        let IPsToScan    = [];
         if (validator.isIP(tmpHost, '4') === true)
         {
             IPsToScan.push(tmpHost);
@@ -184,7 +197,7 @@ module.exports = function(RED) {
         }
         else if (validator.isFQDN(tmpHost) === true)
         {
-            dns.resolve(tmpHost, function(err, addresses){
+            dns.resolve(tmpHost, (err, addresses) => {
                 if (err) {
                     cb("Error resolving host: " + err.message);
                     return;
