f1167cd90ff52364688307827d8f07d3dc241fe0
Sven <mailsvb@gmail.com>
2017-06-21 11:45:24 +0200

    update install file

--- testssl/install-testssl.js
+++ testssl/install-testssl.js
@@ -1,7 +1,7 @@
-var https = require('https');
-var fs = require('fs');
+const https = require('https');
+const fs = require('fs');
 
-var testsslOPTIONS = {
+let testsslOPTIONS = {
     hostname: 'raw.githubusercontent.com',
     port: 443,
     path: '/drwetter/testssl.sh/2.9dev/testssl.sh',
@@ -9,33 +9,27 @@ var testsslOPTIONS = {
     rejectUnauthorized: false
 };
 
-var testsslREQ = https.request(testsslOPTIONS, function(res) {
+let testsslREQ = https.request(testsslOPTIONS, (res) => {
     if (res.statusCode != 200)
     {
         console.error('unable to download testssl.sh script. Please copy it manually to here: ' + __dirname);
     }
     
-    var data = ""
+    let data = "";
     
-    res.on('data', function(d){
-        data += d.toString();
-    });
+    res.on('data', (d) => data += d.toString());
     
-    res.on('end', function(){
-        fs.writeFileSync(__dirname + '/testssl.sh', data);
+    res.on('end', () => {
+        fs.writeFileSync(__dirname + '/testssl.sh', data, {flag: 'w+'});
         fs.chmodSync(__dirname + '/testssl.sh', '755');
         console.log('A copy of testssl.sh has been placed here: ' + __dirname);
     });
 });
 
-
-testsslREQ.on('error', function(e) {
-    console.error('unable to download testssl.sh script. Please copy it manually to here: ' + __dirname);
-});
-
+testsslREQ.on('error', (e) => console.error('unable to download testssl.sh script. Please copy it manually to here: ' + __dirname));
 testsslREQ.end();
 
-var testsslLicOPTIONS = {
+let testsslLicOPTIONS = {
     hostname: 'raw.githubusercontent.com',
     port: 443,
     path: '/drwetter/testssl.sh/2.9dev/LICENSE',
@@ -43,28 +37,23 @@ var testsslLicOPTIONS = {
     rejectUnauthorized: false
 };
 
-var testsslLicREQ = https.request(testsslLicOPTIONS, function(res) {
+let testsslLicREQ = https.request(testsslLicOPTIONS, (res) => {
     if (res.statusCode != 200)
     {
         console.error('unable to download testssl.sh License. Please copy it manually to here: ' + __dirname);
     }
     
-    var data = ""
+    let data = "";
     
-    res.on('data', function(d){
-        data += d.toString();
-    });
+    res.on('data', (d) => data += d.toString());
     
-    res.on('end', function(){
-        fs.writeFileSync(__dirname + '/LICENSE-testssl', data);
+    res.on('end', () => {
+        fs.writeFileSync(__dirname + '/LICENSE-testssl', data, {flag: 'w+'});
         fs.chmodSync(__dirname + '/LICENSE-testssl', '644');
         console.log('By using this node-red node, you agree to the license of testssl.sh. A copy of the license is available here: ' + __dirname);
     });
 });
 
 
-testsslLicREQ.on('error', function(e) {
-    console.error('unable to download testssl.sh script. Please copy it manually to here: ' + __dirname);
-});
-
+testsslLicREQ.on('error', (e) => console.error('unable to download testssl.sh script. Please copy it manually to here: ' + __dirname));
 testsslLicREQ.end();
